e451494f157206ef4c873504f289f41b11fe89db
Ryo <wh404@outlook.com>
2021-10-07 22:50:49 +0100

    Display all books

---
+++ js/script.js
@@ -0,0 +1,64 @@
+let myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? "read" : "not read yet"}`;
+    };
+}
+
+function addBookToLibrary(title, author, pages, hasRead) {
+    //take user’s input and store the new book objects into an array
+    const newBook = new Book(title, author, pages, hasRead);
+    myLibrary.push(newBook);
+}
+
+function showAllBooks() {
+    //loops through the array and displays each book on the page. 
+    //can display them in some sort of table, or each on their own “card”. 
+    for (let i=0; i<myLibrary.length; i++) {
+        // console.table(myLibrary[i]);
+        const books = document.querySelector(".books");     
+        const book = document.createElement("div");
+        book.classList.add("book");
+        const title = document.createElement("p");
+        title.classList.add("title")
+        const author =  document.createElement("p");
+        const pages = document.createElement("p");
+        const hasRead = document.createElement("p");
+        
+        title.textContent = myLibrary[i].title;
+        author.textContent = myLibrary[i].author;
+        pages.textContent = myLibrary[i].pages;
+        hasRead.textContent = myLibrary[i].hasRead;
+
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(pages);
+        book.appendChild(hasRead);
+
+        books.appendChild(book);
+    }
+}
+
+
+addBookToLibrary("title1", "author1", "pages1", true);
+addBookToLibrary("title2", "author2", "pages2", false);
+addBookToLibrary("title3", "author3", "pages3", true);
+
+addBookToLibrary("title1", "author1", "pages1", true);
+addBookToLibrary("title2", "author2", "pages2", false);
+
+
+addBookToLibrary("title3", "author3", "pages3", true);
+addBookToLibrary("title1", "author1", "pages1", true);
+addBookToLibrary("title2", "author2", "pages2", false);
+addBookToLibrary("title3", "author3", "pages3", true);
+addBookToLibrary("title1", "author1", "pages1", true);
+addBookToLibrary("title2", "author2", "pages2", false);
+addBookToLibrary("title3", "author3", "pages3", true);
+
+showAllBooks();
